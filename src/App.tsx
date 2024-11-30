0abd2ec0122f7c57c4727ec30776f58443f8923b
Homvi <honvedoadamkj@gmail.com>
2024-11-30 15:29:40 +0100

    refactor: remove placeholder lorem ipsum content from App component
    - Cleared lengthy placeholder text for cleaner layout and improved readability.

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,7 @@ function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
-      <div className="border-t-[0.5px] h-1 border-white/20 w-full p-6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet similique eius quasi repellendus labore? Dicta
-        dolores vel molestias voluptas, quas corporis saepe maiores minus, praesentium, nesciunt molestiae nihil
-        doloremque inventore! Laborum facere illo, nostrum perspiciatis sequi adipisci soluta ab asperiores recusandae
-        quo in ea sed illum quos? Ad, numquam quae, temporibus exercitationem deleniti vitae quis deserunt harum fugiat
-        ipsum veritatis. Nihil nobis quaerat quos provident magnam rem dicta quibusdam, eaque voluptate molestiae eum
-        enim omnis accusamus doloremque. Libero praesentium, alias consequuntur voluptates reiciendis, fuga quae, beatae
-        quod ab temporibus eaque! Dolor obcaecati amet voluptates repellendus ex, officiis ea voluptatem quasi rerum
-        esse ipsam architecto enim dolore reiciendis quibusdam delectus, in tenetur, excepturi itaque temporibus
-        pariatur quidem inventore voluptatum hic. Rerum! Commodi officiis eius suscipit est corporis molestias quidem
-        doloremque distinctio voluptates deleniti, quibusdam nesciunt magni, quos tempora placeat, voluptatibus beatae
-        magnam fugiat repellendus ipsam pariatur ad modi. Sed, eius delectus. Perspiciatis, voluptatum? Reprehenderit
-        cupiditate placeat ea voluptatibus quas molestias vero cum aperiam, porro maiores nulla amet et! Excepturi
-        perspiciatis magni ea molestias unde, at amet rerum eveniet sunt? Accusamus, dolorem. Omnis voluptatem corrupti
-        ab, deserunt laboriosam, similique saepe eos, quia eaque nam excepturi hic incidunt amet! Provident odit animi
-        alias quos praesentium, minus perspiciatis ab veniam sed a asperiores facere. Itaque voluptas aliquam ipsa
-        quidem, esse in, id laborum eveniet asperiores alias sit, illum nostrum quam cum modi harum quaerat maiores et
-        magni ullam delectus quos sapiente? Pariatur, recusandae autem. Quidem nobis sit quibusdam libero enim fugiat,
-        Error illo facere sint obcaecati eveniet reiciendis, amet voluptatem possimus vel quo nulla quae eum at minus,
-        enim sunt nemo iusto. Voluptas, sapiente explicabo? Fuga repellat ullam nisi molestiae, facere est veniam animi
-        excepturi eos odit eligendi at, necessitatibus quod neque ipsum natus nulla quia quaerat debitis, enim ratione
-        labore laborum ipsa molestiae, facilis, quos maxime inventore perferendis nemo. Aperiam vero nulla cu corporis
-        deleniti! Vero neque molestiae eum! Blanditiis doloremque quia facere accusantium ducimus impedit quod
-        voluptatibus distinctio excepturi veritatis animi voluptatem nemo amet iusto vitae beatae modi labore rerum
-        vero, adipisci repellendus nam. Animi, soluta maxime impedit dolore mollitia possimus distinctio natus voluptas
-        quisquam sint in dicta culpa pariatur corrupti ipsam, eveniet porro repellendus a dignissimos, veniam
-        reprehenderit? Animi ullam cupiditate autem eius. Quasi molestias animi eveniet sed fuga minus labore autem
-        aperiam ab fugiat exercitationem dicta nesciunt, dolorum perspiciatis, quidem blanditiis architecto suscipit.
-        Nisi debitis exercitationem ad eligendi dolorum sed fuga nobis? Eos cum culpa dicta suscipit atque inventore
-        natus vero. Minus vitae asperiores, consectetur voluptatibus sequi quod nulla debitis assumenda eaque, minima
-        possimus quo nisi eveniet molestias? Earum itaque iste numquam. Doloribus ex reprehenderit sapiente eius facilis
-        quibusdam sint assumenda magnam sed quasi, nisi molestiae ea quidem delectus dignissimos inventore illo libero,
-        alias et non mollitia unde excepturi quis. Explicabo, officiis.
-      </div>
+      <div className="border-t-[0.5px] h-1 border-white/20 w-full p-6"></div>
     </div>
   );
 }
