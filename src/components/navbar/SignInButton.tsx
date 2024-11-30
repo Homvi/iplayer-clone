ac311f133ee61f9fe7c64f784a5687e012b80aa2
Homvi <honvedoadamkj@gmail.com>
2024-11-30 15:34:25 +0100

    fix(signin-button): change to button element for improved semantics and accessibility
    - Replaced the `div` wrapper with a `button` element for better semantic HTML and accessibility.
    - Added `group-focus` classes for improved focus styles on focus.

--- src/components/navbar/SignInButton.tsx
+++ src/components/navbar/SignInButton.tsx
@@ -2,15 +2,15 @@ import { User } from 'lucide-react';
 
 const SignInButton = () => {
   return (
-    <div className="flex">
+    <button className="flex group items-center">
       <div className="flex flex-col group ml-3 w-fit">
         <div className="flex gap-3 h-full py-3 cursor-pointer font-semibold items-center min-w-52">
           <User /> Sign in
         </div>
-        <div className="h-[2px] flex w-5 opacity-0 bg-blue-400 group-hover:w-full group-hover:opacity-100 transition-all max-w-[95%] duration-300"></div>
+        <div className="h-[2px] flex w-5 opacity-0 bg-blue-400 group-hover:w-full group-hover:opacity-100 transition-all max-w-[95%] duration-300 group-focus:opacity-100  group-focus:w-full"></div>
       </div>
       <div className="bg-white/20 h-7 my-auto w-[0.5px]"></div>
-    </div>
+    </button>
   );
 };
 
