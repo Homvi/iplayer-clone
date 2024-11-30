3114b343ac2244e10eee4b78b2e8075f8baddcfa
Homvi <honvedoadamkj@gmail.com>
2024-11-30 15:34:03 +0100

    style(navmenu): improve accessibility with focus-visible styles
    - Added `group-focus` classes to enhance focus visibility for better keyboard navigation support.

--- src/components/navbar/NavMenuElement.tsx
+++ src/components/navbar/NavMenuElement.tsx
@@ -7,7 +7,7 @@ const NavMenuElement = ({ url, title }: NavMenuElementProps) => {
   return (
     <a href={url} className="flex flex-col group mx-3 cursor-pointer">
       <div className="flex h-full  font-semibold py-3 items-center">{title}</div>
-      <div className="h-[2px] flex w-5 opacity-0 bg-white group-hover:w-full group-hover:opacity-100 transition-all duration-300"></div>
+      <div className="h-[2px] flex w-5 opacity-0 bg-white group-hover:w-full group-hover:opacity-100 transition-all duration-300 group-focus:opacity-100  group-focus:w-full"></div>
     </a>
   );
 };
