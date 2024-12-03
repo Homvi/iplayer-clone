bac1360e10d21ed17175be825ba34766f1c64dd2
Homvi <honvedoadamkj@gmail.com>
2024-12-03 16:15:16 +0100

    feat: add HeroSection component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import HeroSection from './components/HeroSection/HeroSection';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
-      <div className="border-t-[0.5px] h-1 border-white/20 w-full p-6"></div>
+      <HeroSection />
     </div>
   );
 }
