d55b26e5617701c393ec10a577edf59d8239d00b
Homvi <honvedoadamkj@gmail.com>
2024-12-03 16:42:48 +0100

    feat(hero-section): add functionality to dynamically switch active movie
    - Introduced `useState` to track and update the active movie's index.
    - Modified the HeroSection component to display details of the selected movie dynamically.
    - Added interactive thumbnail grid with scaling and border effects for active selection.
    - Updated the main image, title, and description to reflect the selected movie.
    - Improved user experience with responsive transitions and styling.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,16 +1,20 @@
 // src/components/HeroSection/HeroSection.js
 import { Film, Popcorn } from 'lucide-react';
 import { movies } from './movieData';
+import { useState } from 'react';
 
 const HeroSection = () => {
+  const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
+  const activeMovie = movies[activeImageIndex];
+
   return (
-    <div className="border-t-[0.5px] border-white/30 bg-red-300">
+    <div className="border-t-[0.5px] border-white/30">
       <section className="max-w-7xl mx-auto px-3  relative w-full aspect-video">
         <div className="flex h-full flex-col justify-end">
           <div className="flex flex-col gap-3 z-20 max-w-sm">
-            <h1 className="text-2xl font-bold max-w-xs z-10 relative">{movies[0].title}</h1>
-            <p className="text-gray-300">{movies[0].description}</p>
-            <p className="text-lg font bold">{movies[0].isSeries ? movies[0].updateFrequency : ''}</p>
+            <h1 className="text-2xl font-bold max-w-xs z-10 relative">{activeMovie.title}</h1>
+            <p className="text-gray-300">{activeMovie.description}</p>
+            <p className="text-lg font bold">{activeMovie.isSeries ? activeMovie.updateFrequency : ''}</p>
             {/* button container */}
             <div className="flex gap-3 font-bold">
               <div className="relative h-14 group">
@@ -39,16 +43,28 @@ const HeroSection = () => {
           {/* cover image */}
           <div className="h-fit w-5/6 absolute right-0 top-0">
             <div className="bg-black/90 h-full w-full z-20">
-              <img className="w-full absolute right-0 top-0 aspect-video" src={movies[0].image} alt={movies[0].title} />
+              <img
+                className="w-full absolute right-0 top-0 aspect-video"
+                src={activeMovie.image}
+                alt={activeMovie.title}
+              />
             </div>
           </div>
           {/* thumbnails grid */}
+          {/*  TODO: change images automatically */}
           <div className="h-1/3 bg-gradient-to-b from-transparent via-black to-black relative z-10 py-11">
-            <div className="flex w-full h-full absolute gap-6 max-w-lg">
-              <div className="w-72 aspect-video bg-white"></div>
-              <div className="w-72 aspect-video bg-white"></div>
-              <div className="w-72 aspect-video bg-white"></div>
-              <div className="w-72 aspect-video bg-white"></div>
+            <div className="flex justify-center left-0 gap-6">
+              {movies.map((movie, index) => (
+                <div
+                  onClick={() => setActiveImageIndex(index)}
+                  key={movie.title}
+                  className={`h-32 aspect-video transition-all duration-300 cursor-pointer border-2 ${
+                    activeImageIndex === index ? 'scale-110 border-white' : 'scale-100 border-transparent'
+                  }`}
+                >
+                  <img src={movie.thumbnail} alt={movie.title} className="h-full w-full object-cover" />
+                </div>
+              ))}
             </div>
           </div>
         </div>
