f1f6c61d562a6f2d6aa7decac58f507fc051a41e
Homvi <honvedoadamkj@gmail.com>
2024-12-04 20:13:45 +0100

    feat(movie-card): enhance hover functionality with detailed movie info
    - Added hover effects to replace the cover image with detailed movie information.
    - Displayed a backdrop image, title, description, and vote average on hover.
    - Improved user interactivity with smooth transitions and delay (`delay-100`).
    - Ensured the content layout is well-structured with appropriate padding and alignment.

--- src/components/Carousel/MovieCard.tsx
+++ src/components/Carousel/MovieCard.tsx
@@ -14,22 +14,40 @@ const MovieCard = ({ movie, indexOfVisibleMovies, index, movieImageBaseUrl }: Mo
 
   // Combine classes
   const containerClass = `
-  max-w-[25%] flex-none p-2 transition-all relative duration-300 transform group z-[100] overflow-visible
-  ${isHighLighted ? ' opacity-100 hover:scale-[115%] hover:z-[101] ' : 'opacity-20'}
-  ${originClass}
-`;
+    max-w-[25%] flex-none p-2 transition-all relative duration-300 transform group z-[100] overflow-visible cursor-pointer
+    ${isHighLighted ? 'opacity-100 hover:scale-[115%] hover:z-[101]' : 'opacity-20'}
+    ${originClass}
+  `;
 
   return (
-    <>
-      <div className={containerClass}>
+    <div className={containerClass}>
+      {/* Hover effect container */}
+      <div className="relative h-full w-full group">
+        {/* Cover Image */}
         <img
           src={`${movieImageBaseUrl}${movie.poster_path}`}
           alt={movie.title}
-          className={isHighLighted ? 'group-hover:border-2' : ''}
+          className="w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-0"
         />
-        <h3 className="font-semibold mt-2">{movie.title}</h3>
+
+        {/* Hover content */}
+        <div className="absolute inset-0 bg-black border-2 w-full flex flex-col items-center justify-start opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100">
+          {/* Poster Image */}
+          <img
+            src={`${movieImageBaseUrl}${movie.backdrop_path}`}
+            alt={movie.title}
+            className="w-full mb-4 object-cover"
+          />
+          <div className="flex flex-col justify-center text-center p-3">
+            {/* Title */}
+            <h3 className="text-xl font-bold text-white mb-2">{movie.title}</h3>
+            {/* Description */}
+            <p className="text-sm text-gray-300 text-center line-clamp-6">{movie.overview}</p>
+          </div>
+          <div className="mt-auto text-left w-full p-3 font-bold">{movie.vote_average}/10</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
