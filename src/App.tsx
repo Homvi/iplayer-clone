95c85888b5849c7c6fd518962883edccae6999da
Homvi <honvedoadamkj@gmail.com>
2024-12-04 19:53:06 +0100

    feat(app): integrate Backdrop component into the App layout
    - Added `Backdrop` component to the `App` layout with an initial `isOpen` prop set to `false`.
    - Updated the main container to include the `relative` class for layout adjustments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import Backdrop from './components/Backdrop';
 import CarouselSection from './components/Carousel/CarouselSection';
 import HeroSection from './components/HeroSection/HeroSection';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="bg-black min-h-screen text-white">
+    <div className="bg-black min-h-screen text-white relative">
       <Navbar />
       <HeroSection />
       <CarouselSection />
+      <Backdrop isOpen={false} />
     </div>
   );
 }
